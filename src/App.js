e379a345ceb967b334d861a9b61fb4d69d1865de
Chris <oak.st.west@gmail.com>
2019-05-21 16:26:29 -0700

    commit done 5/21/19 4:26pm

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className='App-div1'>
+        <h1 id='App-h1'>
+          A Cool Box
+        </h1>
+        <h3> Nice And Chill </h3>
+        <h4> Can You Still See Me </h4>
+      </div>
+
+      <div className='App-div2'>
+        <h1 className='App-hsecond'>
+          A Much Cooler Box
+          </h1>
+      </div>
+
+      <div className='App-div3'>
+        <h1>The Coolest Box Of All</h1>
+        <h2>I Am Your Leader</h2>
+      </div>
+
+      <div className='App-div4'>
+        <h1>Just A Box</h1>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
+
+
